fbdfe5ea1989b2c938204d7bd8ac9769edaf6483
bmcmahen <ben.mcmahen@gmail.com>
2014-02-20 20:33:45 -0700

    first

---
+++ Readme.md
@@ -0,0 +1,52 @@
+
+# Youtube
+
+  a better youtube api for embedding videos & fetching metadata. It basically automates all of the script fetching, provides a consistent timeupdate event, and applies a duration & currentTime attribute to the object to make it more consistent with html5 vids.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install bmcmahen/youtube
+
+## Example
+
+```javascript
+var YouTube = require('youtube');
+var vid = new YouTube('URL', {
+  height: 300,
+  width: 500,
+  target: 'youtube-targetnode-id'
+});
+
+vid.play();
+vid.pause();
+// check the source...
+```
+
+The YouTube API is pretty bad and relies on the user passing in the id the target dom node where the video will be inserted. Pass in the id without the `#` sign. 
+
+
+## License
+
+  The MIT License (MIT)
+
+  Copyright (c) 2014 <copyright holders>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,163 @@
+/**
+ * Module Dependencies
+ */
+
+var Emitter = require('emitter');
+var loadScript = require('load-script');
+var debug = require('debug')('youtube');
+
+/**
+ * Expose Youtube
+ */
+
+module.exports = YouTube;
+
+/**
+ * YouTube Constructor
+ * 
+ * @param {String} src     
+ * @param {Object} options 
+ */
+
+function YouTube(src, options){
+  this.src = src;
+  this.options = options || {};
+  this.target = this.options.target;
+  this.width = this.options.width || 400;
+  this.height = this.options.height || 300;
+  this.playerVars = this.options.playerVars;
+  this.currentTime = 0;
+  var self = this;
+  if (typeof(YT) === 'undefined') {
+    window.onYouTubeIframeAPIReady = function(){
+      debug('iframe api ready');
+      self.build();
+    };
+    loadScript('//www.youtube.com/iframe_api');
+  } else {
+    this.build();
+  }
+}
+
+Emitter(YouTube.prototype);
+
+/**
+ * Once the script is fetched, build our vid, fetch meta
+ * 
+ * @return {YouTube} 
+ */
+
+YouTube.prototype.build = function(){
+  var self = this;
+
+  var id = this.getId(this.src);
+
+  this.node = new YT.Player(this.target, {
+    width: this.width,
+    height: this.height,
+    videoId: id,
+    playerVars: this.playerVars
+  });
+
+  this.node.addEventListener('onReady', function(){
+    debug('video ready');
+    self.isReady = true;
+    self.bindEvents();
+    self.duration = self.node.getDuration();
+    self.node.playVideo();
+    self.emit('ready');
+  });
+
+  window.youtubeFeedCallback = function(json){
+    self.meta = json && json.data;
+    self.emit('metaDataReceived', json.data);
+    debug('metadata recieved %j', json);
+  };
+
+  loadScript('http://gdata.youtube.com/feeds/api/videos/'+ id +'?v=2&alt=jsonc&callback=youtubeFeedCallback')
+
+  return this;
+};
+
+/**
+ * Bind events
+ * 
+ * @return {YouTube} 
+ */
+
+YouTube.prototype.bindEvents = function(){
+  var self = this;
+  var node = this.node;
+  node.addEventListener('onStateChange', function(state){
+    if (state.data === 1) self.onPlayback();
+    else if (state.data === 0 || state.data === 2) self.onErrorOrPause();
+  });
+  node.addEventListener('onError', this.onErrorOrPause.bind(this));
+  return this;
+};
+
+/**
+ * Clear our playback interval on pause/error
+ */
+
+YouTube.prototype.onErrorOrPause = function(){
+  this.playing = false;
+  if (this.interval) clearInterval(this.interval);
+};
+
+/**
+ * emit current time
+ */
+
+YouTube.prototype.onTimeUpdate = function(){
+  this.currentTime = this.node.getCurrentTime();
+  this.emit('timeupdate', this.currentTime);
+};
+
+/**
+ * pause video
+ */
+
+YouTube.prototype.pause = function(){
+  debug('pause');
+  this.node.pauseVideo(); 
+  this.playing = false;
+  return this;
+};
+
+/**
+ * play video
+ */
+
+YouTube.prototype.play = function(){
+  debug('play');
+  this.node.playVideo();
+  this.playing = true;
+  return this;
+};
+
+/**
+ * emulate timeupdate event for youtube to
+ * match html api.
+ */
+
+YouTube.prototype.onPlayback = function(){
+  debug('on playback');
+  this.playing = true;
+  if (this.interval) clearInterval(this.interval);
+  this.interval = setInterval(this.onTimeUpdate.bind(this), 250);
+  return this;
+};
+
+/**
+ * get youtube id from url string
+ * Thanks to: http://stackoverflow.com/a/9102270/1198166
+ */
+
+YouTube.prototype.getId = function(url){
+  var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[2].length==11){
+    return match[2];
+  }
+};
